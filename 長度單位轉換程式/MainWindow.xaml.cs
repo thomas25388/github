2d2a21f0000f07c4cc691fc1fdccb67df3185c71
thomas25388 <thomas021929@gmail.com>
2023-03-01 00:02:30 +0800

    長度單位轉換程式

---
+++ 長度單位轉換程式/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 長度單位轉換程式
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtCM_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douCM; //宣告一個double變數，變數名稱叫douCM
+            douCM = Convert.ToDouble(txtCM.Text); //從txtCM輸入文字框取得輸入的文字，並且轉換成double的資料型態
+
+            //透過string.Format格式化成小數點後共10位的數字，轉型成文字型態，在txtM顯示結果
+            txtM.Text = string.Format("{0:0.##########}", douCM / 100);
+            txtKM.Text = string.Format("{0:0.##########}", douCM / 100000);
+            txtIn.Text = string.Format("{0:0.##########}", douCM / 2.54);
+            txtFt.Text = string.Format("{0:0.##########}", douCM / 30.48);
+            txtYard.Text = string.Format("{0:0.##########}", douCM / 91.44);
+        }
+    }
+}
