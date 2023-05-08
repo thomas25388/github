66d1c62277f8c2fe9d0aa55523213fbd76393181
thomas25388 <thomas021929@gmail.com>
2023-05-08 14:31:40 +0800

    登入系統

---
+++ 登入系統/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 登入系統
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlogin_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtusername.Text;
+            string password = txtpassword.Text;
+            if (username == "abc" && password == "123")
+            {
+                txtmessage.Text = "登入成功";
+            }
+            else
+            {
+                txtmessage.Text = "登入失敗";
+            }
+        }
+    }
+}
