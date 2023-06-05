2850fe61f1adda6cc99f85dfe20339853411c374
thomas25388 <thomas021929@gmail.com>
2023-06-05 16:40:37 +0800

    Timer

--- Timer/MainWindow.xaml.cs
+++ Timer/MainWindow.xaml.cs
@@ -20,9 +20,22 @@ namespace Timer
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> hours = new List<string>();            // 小時清單
+        List<string> minutes = new List<string>();          // 分鐘清單
         public MainWindow()
         {
             InitializeComponent();
+            // 建立小時的清單，數字範圍為00-23
+            for (int i = 0; i <= 23; i++)
+                hours.Add(string.Format("{0:00}", i));
+            // 建立分鐘的清單，數字範圍為00-59
+            for (int i = 0; i <= 59; i++)
+                minutes.Add(string.Format("{0:00}", i));
+
+            // 設定小時下拉選單的選單內容
+            cmbHour.ItemsSource = hours;
+            // 設定分鐘下拉選單的選單內容
+            cmbMin.ItemsSource = minutes;
         }
     }
 }
